b4b5d5cb27fdecb4373b2441aa92d399b043b8b2
harsh <harshrpatel93@gmail.com>
2019-06-07 10:16:26 -0400

    made a few components"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Notes from './pages/Note';
+import AddUpdateNote from './pages/AddUpdateNote';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Notes} />
+            <Route exact path="/add" component={AddUpdateNote} />
+            <Route exact path="/update/:id" component={AddUpdateNote} />
+            {/* set up default/404, this must come last */}
+            <Route render={() => <h1>404 bro</h1>} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
